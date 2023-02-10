83270cb7ace4acf7ab0cf5cdff235c3d46c10f29
Nadeem Ahmad <nadeemgoogl786@gmail.com>
2023-02-10 10:05:39 +0530

    first commit

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         mongoose.set("strictQuery", false);
-        await mongoose.connect(`mongodb://localhost:27017/googleAuthDB`, {
+        await mongoose.connect(`mongodb+srv://Nadeem-Ahmad786:gNAzmEubcDUQmWY@cluster0.xyoorkm.mongodb.net/ChatGPTDB?retryWrites=true&w=majority`, {
             useNewUrlParser: true
           });
         console.log(`MongoDB Connected...`);
